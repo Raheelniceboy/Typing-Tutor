53337965ab32022d83d3402261247b4c00703268
Giangnt <nguyentruonggiang71096@gmail.com>
2016-10-21 22:26:04 +0700

    Add existing file

--- js/results.js
+++
@@ -1,5 +0,0 @@
-function successDialog(data) {
-    var message = "You finished this exercise in " + data.time + "; and your accuracy is " + data.accuracy + "%!";
-    $("#modal-message").text(message);
-    $("#modal-success").modal("show");
-}
